335263960e8b071f3e1150fa618b3e61318f8751
kei12131111 <kei12131111@gmail.com>
2021-06-07 20:43:41 +0900

    新規プロジェクト作成

---
+++ spsaml/src/main/java/com/saml/spsaml/SpsamlApplication.java
@@ -0,0 +1,14 @@
+package com.saml.spsaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsamlApplication {
+
+	//sample
+	public static void main(String[] args) {
+		SpringApplication.run(SpsamlApplication.class, args);
+	}
+
+}
---
+++ spsaml/src/test/java/com/saml/spsaml/SpsamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saml.spsaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpsamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
